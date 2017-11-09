c80b773fb438794f9adca67994c942bca00eafbc
Gritco Andreea <gritco.andreea@gmail.com>
2017-11-09 22:10:17 +0200

    Created list with items in React-Native
    Created a new component in which I store my list.

--- REACT-NATIVE/App.js
+++ REACT-NATIVE/App.js
@@ -12,6 +12,8 @@ import {
   View
 } from 'react-native';
 
+import ListComponent from './app/ListComponent';
+
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' +
     'Cmd+D or shake for dev menu',
@@ -22,17 +24,9 @@ const instructions = Platform.select({
 export default class App extends Component<{}> {
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
-      </View>
+        <View>
+          <ListComponent/>
+        </View>
     );
   }
 }
---
+++ REACT-NATIVE/app/ListComponent.js
@@ -0,0 +1,141 @@
+import React, {Component} from 'react';
+import {AppRegistry, Text, View,ListView,StyleSheet} from 'react-native';
+
+
+const products = [
+    {
+        description: 'Fond de ten Born This Way',
+        productType: 'FOND_DE_TEN',
+        quantity: '2',
+        price: '180',
+        brand: 'Too Faced'
+    },
+    {
+        description: 'Highlighter',
+        productType: 'ILUMINATOR',
+        quantity: '1',
+        price: '200',
+        brand: 'Anastasia Beverly Hills'
+    },
+    {
+        description: 'Pudra compacta',
+        productType: 'PUDRA_COMPACTA',
+        quantity: '1',
+        price: '35',
+        brand: 'Rimmel London'
+    },
+    {
+        description: 'Rimel',
+        productType: 'RIMEL',
+        quantity: '1',
+        price: '23',
+        brand: 'Melkior'
+    },
+    {
+        description: 'Ruj Baton',
+        productType: 'RUJ',
+        quantity: '1',
+        price: '90',
+        brand: 'MAC'
+    },
+    {
+        description: 'Creion de buze nuanta 06 Satin Mauve',
+        productType: 'CREION_DE_BUZE',
+        quantity: '1',
+        price: '15',
+        brand: 'Essence'
+    },
+    {
+        description: 'Fond de ten nuanta 02',
+        productType: 'FOND_DE_TEN',
+        quantity: '1',
+        price: '50',
+        brand: 'Borjouis'
+    },
+    {
+        description: 'Paleta de farduri Naked 3',
+        productType: 'FARD_DE_OCHI',
+        quantity: '1',
+        price: '234',
+        brand: 'Urban Decay'
+    },
+    {
+        description: 'Rimel Colossal',
+        productType: 'RIMEL',
+        quantity: '3',
+        price: '30',
+        brand: 'Maybelline New York'
+    },
+    {
+        description: 'Ruj Soft Matte nuanta Copenhagen',
+        productType: 'RUJ',
+        quantity: '1',
+        price: '70',
+        brand: 'NYX'
+    },
+    {
+        description: 'Creion de sprancene nuanta Dark Brown',
+        productType: 'CREION_DE_SPRANCENE',
+        quantity: '1',
+        price: '11',
+        brand: 'Essence'
+    }
+
+
+]
+
+
+export default class ListComponent extends Component{
+
+    constructor(){
+        super();
+        const ds =  new ListView.DataSource({rowHasChanged : (r1,r2) => r1 !== r2});
+        this.state={
+            productDataSource : ds.cloneWithRows(products),
+        };
+    }
+
+    renderRow(product, sectionId, rowId,highlightRow){
+
+      return(  <View style={styles.container}>
+            <Text style={styles.headline}>{product.description}</Text>
+            <Text style={styles.details}>{product.brand}</Text>
+        </View>
+      )
+    }
+
+    render(){
+        return(
+            <ListView
+                dataSource={this.state.productDataSource}
+                renderRow={this.renderRow.bind(this)}
+            />
+        );
+    }
+
+
+}
+
+const styles = StyleSheet.create({
+
+    container:{
+        flex:1,
+        padding:25,
+        backgroundColor:'#f4f4f4',
+        marginBottom:3
+    },
+
+    details: {
+        //fontSize: 14,
+        marginBottom: 8
+    },
+    headline: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        marginBottom: 8,
+        flex: 1
+    }
+
+});
+
+AppRegistry.registerComponent('ListComponent',() => ListComponent );
